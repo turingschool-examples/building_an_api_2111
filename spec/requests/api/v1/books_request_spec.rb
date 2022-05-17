28c9d507e447ff421289b1514f1a4b1491eb8ff7
Juliet Eyraud <juliet@turing.edu>
2022-05-17 11:17:59 -0500

    Add hand-rolled serializer for books

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe 'The books API' do
 
     get '/api/v1/books'
 
-    books = JSON.parse(response.body, symbolize_names: true)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    books = response_body[:data]
 
     expect(response).to be_successful
     expect(books.count).to eq(3)
@@ -15,20 +16,24 @@ RSpec.describe 'The books API' do
       expect(book).to have_key(:id)
       expect(book[:id]).to be_an(Integer)
 
-      expect(book).to have_key(:title)
-      expect(book[:title]).to be_a(String)
+      expect(book[:type]).to eq('book')
 
-      expect(book).to have_key(:author)
-      expect(book[:author]).to be_a(String)
+      expect(book[:attributes]).to have_key(:title)
+      expect(book[:attributes][:title]).to be_a(String)
 
-      expect(book).to have_key(:genre)
-      expect(book[:genre]).to be_a(String)
+      expect(book[:attributes]).to have_key(:author)
+      expect(book[:attributes][:author]).to be_a(String)
 
-      expect(book).to have_key(:summary)
-      expect(book[:summary]).to be_a(String)
+      expect(book[:attributes]).to have_key(:genre)
+      expect(book[:attributes][:genre]).to be_a(String)
 
-      expect(book).to have_key(:number_sold)
-      expect(book[:number_sold]).to be_an(Integer)
+      expect(book[:attributes]).to have_key(:summary)
+      expect(book[:attributes][:summary]).to be_a(String)
+
+      expect(book[:attributes]).to have_key(:number_sold)
+      expect(book[:attributes][:number_sold]).to be_an(Integer)
+
+      expect(book[:attributes]).to_not include(:created_at)
     end
   end
 end
